04d05b4fff655877d435e53a9a31e25569653c9f
Shaan Patel <shaanharvard@gmail.com>
2022-03-01 15:35:18 -0500

    finished control.py, fixed 0-180 error in servo,py

---
+++ pi/src/control.py
@@ -0,0 +1,48 @@
+#!/home/pi/.pyenv/shims/python
+
+import readchar
+from servo import servo_motor
+import RPi.GPIO as GPIO
+
+ANGLE_STEP = 5
+DEFAULT_ANGLE = 90
+
+GPIO_DRIVE_PIN = 27
+DRIVE_FORWARD = 1
+DRIVE_STOP = 0
+
+# init drive motor
+GPIO.setmode(GPIO.BCM) 			# choose BCM to use GPIO numbers instead of pin numbers
+GPIO.setup(GPIO_DRIVE_PIN, GPIO.OUT)    # change to output
+
+# init servo steering
+s = servo_motor()
+angle = DEFAULT_ANGLE 
+s.spin(angle)
+
+while True:
+	keypress = readchar.readkey()
+
+	if keypress == readchar.key.UP:
+		GPIO.output(GPIO_DRIVE_PIN, DRIVE_FORWARD)
+		print('Drive')
+
+	if keypress == readchar.key.DOWN:
+		GPIO.output(GPIO_DRIVE_PIN, DRIVE_STOP)
+		print('Stop')
+
+	if keypress == readchar.key.LEFT:
+		print('Turn Left')
+		angle += ANGLE_STEP
+		s.spin(angle)
+
+	if keypress == readchar.key.RIGHT:
+		print('Turn Right')
+		angle -= ANGLE_STEP
+		s.spin(angle)
+
+	if keypress == readchar.key.CTRL_C:
+		print("Quit...")
+		GPIO.output(GPIO_DRIVE_PIN, DRIVE_STOP)
+		GPIO.cleanup()
+		break
